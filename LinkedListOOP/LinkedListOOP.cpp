7fa9acef656e195b35c3506b81d8e8734e41cc6d
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-19 15:15:03 +0700

    selesai membuat class List

--- LinkedListOOP/LinkedListOOP.cpp
+++ LinkedListOOP/LinkedListOOP.cpp
@@ -8,3 +8,14 @@ public:
 	char nama[20];
 	Node* next;
 };
+
+class List {
+	Node* START;
+public:
+	List();
+	void addNode();
+	bool Search(int nim, Node** curent, Node** previous);
+	bool listEmpty();
+	bool delNode(int element);
+	void traverse();
+};
